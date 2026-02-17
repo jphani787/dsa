c820a7dc770efbc8c9bc24c04d565ace02084645
jphani787 <jphanindra787@gmail.com>
2026-02-17 11:46:41 +0530

    largest & second  number

---
+++ largestNumber.js
@@ -0,0 +1,16 @@
+const arr = [4, 8, 0, 2, 9, 7, 1];
+function largestNumber() {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] !== largest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+
+console.log(largestNumber());
