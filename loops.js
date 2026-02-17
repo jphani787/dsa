bea327e2d8f2ce041b22c5ab45ec3981a5c79659
jphani787 <jphanindra787@gmail.com>
2026-02-17 09:28:39 +0530

    largest number

--- loops.js
+++ loops.js
@@ -1,1 +1,39 @@
-.
+// let arr = [20, 12, 23, 34, 45, 56, 67, 78, 89, 90];
+
+// // for loop
+// function forLoop(search) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === search) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(forLoop(34));
+
+// let arr = [10, -12, 20, -23, 34, -45, 56, -67, 78, -89];
+
+// function nagativeNumbers() {
+//   let negativeCount = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < 0) {
+//       negativeCount++;
+//     }
+//   }
+//   return negativeCount;
+// }
+// console.log(nagativeNumbers());
+
+// const arr = [-10, -2, -3, -4, -7, -9];
+// function largestNumber() {
+//   let largestNumber = -Infinity;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > largestNumber) {
+//       largestNumber = arr[i];
+//     }
+//   }
+//   return largestNumber;
+// }
+
+// console.log(largestNumber());
