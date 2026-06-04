26cde7b215902ab240c3e863be93ca74669ce53b
jphani787 <jphanindra787@gmail.com>
2026-06-05 00:32:43 +0530

    maxProfit added

---
+++ maxProfit.js
@@ -0,0 +1,17 @@
+function maxProfit(prices) {
+  let min = prices[0];
+  let maxProfit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - min > maxProfit) {
+      maxProfit = prices[i] - min;
+    }
+
+    if (prices[i] < min) {
+      min = prices[i];
+    }
+  }
+  return maxProfit;
+}
+
+const prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices)); // Output: 5
