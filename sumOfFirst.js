5781bc49e4d8d6fdbe120c783384f64861a46c08
jphani787 <jphanindra787@gmail.com>
2026-06-06 22:59:35 +0530

    sum of all elements

--- sumOfFirst.js
+++ sumOfFirst.js
@@ -1,8 +1,15 @@
-function sumOfFirst(n) {
-  if (n === 0) return 0;
+// function sumOfFirst(n) {
+//   if (n === 0) return 0;
+//   return n + sumOfFirst(n - 1);
+// }
+// console.log(sumOfFirst(5)); // 15
 
-  console.log(n);
-  return n + sumOfFirst(n - 1);
+let nums = [5, 3, 2, 0, 1];
+function sumOfAllElements(n) {
+  if (n === 0) return nums[0];
+  let f = nums[n] % 2 !== 0 ? nums[n] : 0;
+  f = f + sumOfAllElements(n - 1);
+  return f;
 }
 
-console.log(sumOfFirst(5)); // 15
+console.log(sumOfAllElements(nums.length - 1)); // 15
