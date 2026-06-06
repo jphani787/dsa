f46923f18787b6c2cd59736f6b90fa7c4afbfae1
jphani787 <jphanindra787@gmail.com>
2026-06-07 00:52:09 +0530

    added fib number

---
+++ fib.js
@@ -0,0 +1,16 @@
+function fib(n) {
+  //   if (n <= 1) return n;
+  //   let f = fib(n - 1) + fib(n - 2);
+  //   console.log(n, f);
+  //   return f;
+  //   let nums = [0, 1];
+  //   let count = 0;
+  //   for (let i = 2; i <= n; i++) {
+  //     nums[i] = nums[i - 1] + nums[i - 2];
+  //     count++;
+  //   }
+  //   console.log(count);
+  //   return nums;
+}
+// 0 1 1 2 3 5 8 13
+console.log(fib(7)); // 13
