952a3f41606b0ba6faa5433258887c599488a47a
jphani787 <jphanindra787@gmail.com>
2026-06-14 00:40:36 +0530

    added linked list cycle for 2nd approch

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -1,15 +1,30 @@
-function hasCycle(head) {
-  let seenNodes = new Set();
-  let curr = head;
-  while (curr) {
-    if (seenNodes.has(curr)) {
-      return true;
-    }
+// function hasCycle(head) {
+//   let seenNodes = new Set();
+//   let curr = head;
+//   while (curr) {
+//     if (seenNodes.has(curr)) {
+//       return true;
+//     }
 
-    seenNodes.add(curr);
-    curr = curr.next;
+//     seenNodes.add(curr);
+//     curr = curr.next;
+//   }
+//   return false;
+// }
+
+function hasCycle(head) {
+  if (!head) return false;
+  let slow = head;
+  let fast = head.next;
+  while (slow != fast) {
+    if (!fast || !fast.next) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
   }
-  return false;
+  return true;
 }
+
 const head = [3, 2, 0, -4];
 console.log(hasCycle(head));
