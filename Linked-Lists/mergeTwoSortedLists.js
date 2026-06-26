d4e74143050930c77a06e4151916a398634d7178
jphani787 <jphanindra787@gmail.com>
2026-06-26 09:55:55 +0530

    added merge tow sorted list

---
+++ Linked-Lists/mergeTwoSortedLists.js
@@ -0,0 +1,26 @@
+function mergeTwoLists(list1, list2) {
+  let start = new ListNode();
+  let curr = start;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      curr.next = list1;
+      list1 = list1.next;
+    } else {
+      curr.next = list2;
+      list2 = list2.next;
+    }
+
+    curr = curr.next;
+  }
+  // corner cases
+  if (!list1) {
+    curr.next = list2;
+  }
+
+  if (!list2) {
+    curr.next = list1;
+  }
+
+  return start.next;
+}
