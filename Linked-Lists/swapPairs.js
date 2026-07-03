57543649316148ec95ee9f60c2f15c1970b79329
jphani787 <jphanindra787@gmail.com>
2026-07-03 14:15:14 +0530

    swapPairs added approch1

---
+++ Linked-Lists/swapPairs.js
@@ -0,0 +1,22 @@
+function swapPairs(head) {
+  if (!head || !head.next) return head;
+  let sentinel = new ListNode();
+  sentinel.next = head;
+  let p = sentinel;
+  let c = head;
+  let n = head.next;
+  while (c && n) {
+    p.next = n;
+    c.next = n.next;
+    n.next = c;
+
+    p = c;
+    c = p.next;
+    n = c && c.next;
+  }
+  return sentinel.next;
+}
+
+const head = [1, 2, 3, 4];
+// output: [2, 1, 4, 3]
+console.log(swapPairs(head)); // [2,1,4,3]
