57e4618910200b329f891ee44f1455e205824f6d
jphani787 <jphanindra787@gmail.com>
2026-07-04 15:38:20 +0530

    added lengthOfLastWord 2nd approch

--- Strings/lengthOfLastWord.js
+++ Strings/lengthOfLastWord.js
@@ -1,6 +1,25 @@
 function lengthOfLastWord(s) {
-  const sl = s.trim().split(" ");
-  return sl[sl.length - 1].length;
+  //   const sl = s.trim().split(" ");
+  //   return sl[sl.length - 1].length;
+
+  let n = s.length - 1;
+  while (n >= 0) {
+    if (s[n] === " ") {
+      n--;
+    } else {
+      break;
+    }
+  }
+  let nLength = 0;
+  while (n >= 0) {
+    if (s[n] !== " ") {
+      nLength++;
+      n--;
+    } else {
+      break;
+    }
+  }
+  console.log(s.length, n, nLength);
 }
 let s = "   fly me   to   the moon  ";
 console.log(lengthOfLastWord(s));
